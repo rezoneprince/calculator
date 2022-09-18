6f85ed86d54b4e812ca8994732542c0c8d887d58
rezoneprince <rezoneprinceme@gmail.com>
2022-09-19 01:02:34 +0600

    some updated done

--- js/app.js
+++ js/app.js
@@ -1,27 +1,24 @@
-let outputScreen = document.getElementById("outputscreen")
+let outputScreen = document.getElementById("outputscreen");
+
 function display(num) {
     outputScreen.value += num;
 }
+
 function Calculate() {
     try{
-    outputScreen.value = eval(outputScreen.value);
+        outputScreen.value = eval(outputScreen.value);
     }
     catch (err) {
-    alert("Invalid")
+        alert("Invalid");
     }
     
 }
 
-function clear() {
+function Clear() {
     outputScreen.value = "";
 
 }
 function del() {
-    outputScreen.value = outputScreen.value
-        .slice(0, -1);
+    outputScreen.value = outputScreen.value.slice(0, -1);
     
 }
-// function clear() {
-//     outputScreen.value = outputScreen.value
-//     .slice(del);
-// }
