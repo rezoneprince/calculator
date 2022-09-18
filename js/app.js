3efa9286981aeeeac580e3a37b70726ef9c04603
rezoneprince <rezoneprinceme@gmail.com>
2022-09-18 20:50:49 +0600

    js work add

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 let outputScreen = document.getElementById("outputscreen")
+function display(num) {
+    outputScreen.value += num;
+}
+function Calculate() {
+    try{
+    outputScreen.value = eval(outputScreen.value);
+    }
+    catch (err) {
+    alert("Invalid")
+    }
+    
+}
+
+function clear() {
+    outputScreen.value = "";
+
+}
+function del() {
+    outputScreen.value = outputScreen.value
+        .slice(0, -1);
+    
+}
+// function clear() {
+//     outputScreen.value = outputScreen.value
+//     .slice(del);
+// }
